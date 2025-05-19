a62bdc89fe9c13c546d0698404912ac8a9db88b6
zwxdogs <53159397+zwxdogs@users.noreply.github.com>
2025-05-19 10:44:14 +0800

    全局参数

--- global_para.m
+++ global_para.m
@@ -1,22 +1,17 @@
 classdef global_para
-    %GLOBAL_PARA 此处显示有关此类的摘要
-    %   此处显示详细说明
+    % 全局参数
     
-    properties
+    % 参数
+    properties (Access = public)
         fs              % 采样频率
+        c0              % 声速
     end
-    
+
+    % constructor
     methods
-        function obj = global_para(inputArg1,inputArg2)
-            %GLOBAL_PARA 构造此类的实例
-            %   此处显示详细说明
-            obj.Property1 = inputArg1 + inputArg2;
-        end
-        
-        function outputArg = method1(obj,inputArg)
-            %METHOD1 此处显示有关此方法的摘要
-            %   此处显示详细说明
-            outputArg = obj.Property1 + inputArg;
+        function para = global_para(fs, c0)
+            para.fs = fs;
+            para.c0 = c0;
         end
     end
 end
